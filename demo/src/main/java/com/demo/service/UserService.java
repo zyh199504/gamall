18daa6e11160c3d78d0800966b124666b22ca261
JustinBigJerry <40121015+JustinBigJerry@users.noreply.github.com>
2019-07-23 11:55:01 +0800

    日志

--- demo/src/main/java/com/demo/service/UserService.java
+++ demo/src/main/java/com/demo/service/UserService.java
@@ -1,7 +1,5 @@
 package com.demo.service;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 public interface UserService {
---
+++ demo/src/main/java/com/demo/util/WebLogAspect.java
@@ -0,0 +1,45 @@
+package com.demo.util;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class WebLogAspect {
+    private final Logger logger = LoggerFactory.getLogger(WebLogAspect.class);
+
+    @Pointcut("execution(public * com.demo.controller..*.*(..))")
+    public void controller(){}
+
+    @Before("controller()") //在切入点的方法run之前要干的
+    public void logBeforeController(JoinPoint joinPoint) {
+
+
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();//这个RequestContextHolder是Springmvc提供来获得请求的东西
+        HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
+
+        // 记录下请求内容
+        logger.info("################URL : " + request.getRequestURL().toString());
+        logger.info("################HTTP_METHOD : " + request.getMethod());
+        logger.info("################IP : " + request.getRemoteAddr());
+        logger.info("################THE ARGS OF THE CONTROLLER : " + Arrays.toString(joinPoint.getArgs()));
+
+        //下面这个getSignature().getDeclaringTypeName()是获取包+类名的   然后后面的joinPoint.getSignature.getName()获取了方法名
+        logger.info("################CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        //logger.info("################TARGET: " + joinPoint.getTarget());//返回的是需要加强的目标类的对象
+        //logger.info("################THIS: " + joinPoint.getThis());//返回的是经过加强后的代理类的对象
+
+    }
+}
